ea2707bbf970229aedc4f33da13c55a83d4480f8
addy100 <72308696+addy100@users.noreply.github.com>
2021-02-08 23:34:43 +0530

    updates

--- qrcode generator.py
+++ qrcode generator.py
@@ -1,31 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
-
-
+# In[1]:
 ## GENERATE qrCode USING PYTHON
-
 import qrcode
 
-
-# In[6]:
-
-
+# In[2]:
 information = 'Hello! Welcome'
 qr = qrcode.make(information)
 qr.save('Wel.png')
 print ('qr Code is generated.')
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
